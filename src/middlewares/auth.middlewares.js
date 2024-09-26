1d3169a99eb49bcf15151694765ccbdfa4f7b602
Shubhanshu Nayak <devalchamist@gmail.com>
2024-09-26 12:28:42 +0530

    Backend Template

---
+++ src/middlewares/auth.middlewares.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
+
+module.exports.Auth = async (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(401).send({
+      error: true,
+      result: null,
+      message: "UnAuthorized",
+    });
+  }
+  const splitToken = token.split(" ")[1];
+  // console.info({token})
+  if (splitToken && splitToken === "null") {
+    return res.status(401).send({
+      error: true,
+      result: null,
+      message: "UnAuthorized",
+    });
+  }
+  const decodedData = jwt.verify(splitToken, JWT_SECRET);
+  req.user = decodedData;
+
+  console.info("<------------Authentication--------->");
+  console.info({ token, decodedData });
+  console.info("<----------------End---------------->");
+
+  next();
+};
